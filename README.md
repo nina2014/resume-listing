f986a97042d5b7cbb8e832a554f2bc69ab35423e
Nina K Linux <nina2014@chiclancer.com>
2014-03-21 13:09:08 -0700

    first push

---
+++ README.md
@@ -0,0 +1,4 @@
+chiclancer_v3
+=============
+
+A social network for women developers
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PublicV3::Application.config.secret_key_base = 'd36e156868b3c9ab5bc25648ccd02618fa80e083105cb187a99290b5a1ee94b98f953148487043956557c23327f03d5dec44be507c317f0e4e190fc310b16182'
